cd844d66ada8d6045e40af71491badb0ec320628
TeoMan <celik.teo@hotmail.com>
2025-06-22 15:39:57 +0200

    updated go und brake fixed

--- exercises_Lesson_17/car.js
+++ exercises_Lesson_17/car.js
@@ -5,19 +5,20 @@ export class car {
     this.speed= 0; 
   }
   go() {
-    this.speed = Math.min(this.speed += 5 ,200;)}
+    this.speed = Math.min(this.speed += 5 ,200);}
     brake() {
-      this.speed = Math.max( -= 5;0);
+      this.speed = Math.max(this.speed -= 5,0);
     }
     
 
   getDetails() {
-    return `${this.brand} ${this.model}`;
+    return `${this.brand} ${this.model} ${this.speed}`;
   }
 }
-const Toyota = new car("Toyota", "Corolla");
+const Toyota = new car("Toyota", "Corolla", 120);
 const Tesla = new car("Tesla", "Model S");
 
+Toyota.go();
 console.log(Toyota.getDetails(),',',
 
 Tesla.getDetails());
