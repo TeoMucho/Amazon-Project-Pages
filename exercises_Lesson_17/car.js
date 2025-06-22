dde088448576ddfe09dbe65fae2f8c0a226baba3
TeoMan <celik.teo@hotmail.com>
2025-06-22 15:37:34 +0200

    speed attribut mit go und brake functions erstellt.

--- exercises_Lesson_17/car.js
+++ exercises_Lesson_17/car.js
@@ -2,7 +2,14 @@ export class car {
   constructor(brand,model) {
     this.brand = brand;
     this.model = model;
+    this.speed= 0; 
   }
+  go() {
+    this.speed = Math.min(this.speed += 5 ,200;)}
+    brake() {
+      this.speed = Math.max( -= 5;0);
+    }
+    
 
   getDetails() {
     return `${this.brand} ${this.model}`;
@@ -14,4 +21,6 @@ const Tesla = new car("Tesla", "Model S");
 console.log(Toyota.getDetails(),',',
 
 Tesla.getDetails());
+
+
 
